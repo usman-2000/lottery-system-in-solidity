a407e8d7868368da5e23052d9781fed75d642d86
Usman Rahim <80035765+usman-2000@users.noreply.github.com>
2022-10-15 07:34:19 +0500

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -10,15 +10,34 @@ contract LotterySystem{
     }
 
     receive() external payable {
+        require(msg.value >= 1 ether);
         participants.push(payable(msg.sender));
     }
 
     function getBalance() public view returns(uint){
+        require(msg.sender == manager);
         return address(this).balance;
     }
 
-    function sendToUser(uint indx) public {
-        participants[indx].transfer(3 ether);
+    // function sendToUser(uint indx) public {
+    //     participants[indx].transfer(3 ether);
+    // }
+
+    function random() public view  returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+    function selectWinner() public view returns(address){
+        require(msg.sender == manager);
+        require(participants.length>=3);
+
+        uint r = random();
+        uint index = r  % participants.length;
+        address payable winner;
+
+        winner = participants[index];
+        return winner;
+
+
     }
 
 
